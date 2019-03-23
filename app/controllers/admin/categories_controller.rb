4ebf7617c08091c81886c3c846a1e99107c26d07
Patrick O Connor <padraigoc@gmail.com>
2019-03-23 04:34:02 +0000

    Admin login now working

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: "Jungle", password: "Book"
 
 
     # GET /admin/categories (action) #references Category class
